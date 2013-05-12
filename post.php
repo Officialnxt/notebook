e02b06aea3f74034bee91b7961a2637cc19a4126
Officialnxt <esmith233@textfree.us>
2013-05-11 23:33:29 -0400

    Create post.php

---
+++ post.php
@@ -0,0 +1,14 @@
+<form action='post.php' method='post'>
+<input type='text' name='title' placeholder='Title...'><br />
+<textarea name='text' rows='7' cols='35' placeholder='Text...'></textarea>
+<input type='text' name='tags' placeholder='Tags...'><br />
+<input type='submit' name='submit'>
+</form>
+
+<?php
+
+if($_POST['submit']){
+
+}
+
+?>
