62d02b424f1ddb0c4a904f39c5e19295c82dd4a8
Officialnxt <esmith233@textfree.us>
2013-05-11 23:16:41 -0400

    Update blog.php

--- blog.php
+++ blog.php
@@ -4,13 +4,16 @@ require("connect.php");
 
 //Search our database for blog posts. They will be order by the id and limited to 10 posts
 $sql = mysql_query("SELECT * FROM Blog_posts ORDER BY id LIMIT 10");
+//Get the number of posts
 $numrows = mysql_num_rows($sql);
+//Check if there is atleast 1 post
 if($numrows >= 1){
+//while loop for posts
 while($row = mysql_fetch_assoc($sql)){
 
 $id = $row['id'];
 $title = $row['title'];
-$text = $row['text'];
+$text = nl2br($row['text']);
 $tags = $row['tags'];
 $date = $row['date'];
 
